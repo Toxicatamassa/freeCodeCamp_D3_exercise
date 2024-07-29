e3117f57a1df456ed3b8790a4c55dca05e7d9e8a
Dan Ågren <danaagren@gmail.com>
2024-07-29 23:43:56 +0200

    Uppdaterar Episode 6 samt lägger till d3 modulen

--- Episode_6/script.js
+++ Episode_6/script.js
@@ -1,3 +1,24 @@
+import * as D3 from '../node_modules/d3/dist/d3.js';
 
 
+d3.select('body').append('h1').text('Hello, D3!');
 
+// create a new SVG element
+const svg = d3.select('body').append('svg')
+    .attr('width', 500)
+    .attr('height', 500)
+    .style('border', '1px solid black')
+    .style('background-color', 'black');
+
+// create a bar chart
+const dataset = [5, 10, 15, 20, 25];
+
+svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * 30)
+    .attr('y', (d) => 480 - d * 10)
+    .attr('width', 25)
+    .attr('height', (d) => d * 10)
+    .attr('fill', 'blue');
